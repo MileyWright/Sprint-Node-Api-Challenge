ab0824b3bb7d70e35ac10a39b7514f8aed6369dd
Miley Wright <dishaliekwright@yahoo.com>
2020-01-24 21:39:57 -0500

    action routes completed

--- NodeServer/Routes/actionRoutes.js
+++ NodeServer/Routes/actionRoutes.js
@@ -6,18 +6,54 @@ const actionDb = require('../data/helpers/actionModel');
 
 router.use(express.json());
 
-    //  /actions
-    router.get('/', (req, res) => {
-        actionDb.get()
-        .then(action => {
-            res.status(200).json(action)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({errorMessage: "Trouble accessing the actions"})
-        })
+//  /actions
+router.get('/', (req, res) => {
+    actionDb.get()
+    .then(action => {
+        res.status(200).json(action)
     })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({errorMessage: "Trouble accessing the actions"})
+    })
+})
 
+router.get('/:id', validateActionById, (req, res) => {
+    const id = req.params.id;
+    actionDb.get(id)
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({errorMessage: "Could not retrieve specified ID"})
+    })
+})
+
+router.delete("/:id", validateActionById, (req, res) => {
+    const id = req.params.id;
+    actionDb.remove(id)
+    .then(deleted => {
+        res.status(200).json({deleted})
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: 'The action could not be removed'})
+    })
+})
+
+router.put('/:id', validateActionById, validateAction, (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    actionDb.update(id, data)
+    .then( action => {
+        res.status(201).json(action)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ errorMessage: "Action was not updated."})
+    })
+})
 
 //custom middleware
 function validateAction(req,res,next) {
