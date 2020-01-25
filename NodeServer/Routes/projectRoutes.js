ddab93e0890a9ce497e85343823bcddf6c1463f0
Miley Wright <dishaliekwright@yahoo.com>
2020-01-24 21:41:11 -0500

    custom middleware

--- NodeServer/Routes/projectRoutes.js
+++ NodeServer/Routes/projectRoutes.js
@@ -17,4 +17,43 @@ router.get('/', (req, res) => {
     })
 })
 
+
+
+//custom middleware
+function  validateProjectId(req, res, next) {
+    const id = req.params.id;
+    projectDb.get(id)
+        .then(project => {
+            if(!project) {
+                res.status(404).json({error: 'The specified ID does not exist.'})
+            } else {
+                next();
+            }
+        })
+}
+
+function validateProject(req, res, next) {
+    const data = req.body;
+    if(!data){
+        res.status(400).json({message: 'missing user data.'})
+    } else if(!data.name){
+        res.status(400).json({message: 'missing required name field.'})
+    } else if(!data.description) {
+        res.status(400).json({message: 'missing required description field.'})
+    } else {
+        next();
+    }
+}
+
+function validateAction(req, res, next) {
+    const data = req.body;
+    if(!data){
+        res.status(400).json({message: 'missing action data.'})
+    } else if(!data.notes || !data.description){
+        res.status(400).json({message: 'missing required notes and/or description field'})
+    } else {
+        next();
+    }
+}
+
 module.exports = router;
