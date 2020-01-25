7d2244d96e688ccb231bb36ab26efafe588edee8
Miley Wright <dishaliekwright@yahoo.com>
2020-01-25 00:12:35 -0500

    cant seem to fix the npm bug installing more dependencies

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import projectCard from './projectCard';
+// import axios from 'axios';
 import './App.css';
 
 function App() {
+// const [data, setData] = useState([])
+
+// useEffect(() => {
+//   axios
+//   .get('https://node-api-challenge.herokuapp.com/projects')
+//   .then(res => {
+//     console.log(res)
+//     setData(res)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+// }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <projectCard projects = {data} /> */}
       </header>
     </div>
   );
