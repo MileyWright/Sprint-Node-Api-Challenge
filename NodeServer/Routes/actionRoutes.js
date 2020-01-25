30be6cc22db463b5510f7be871d94f73db2fdff9
Miley Wright <dishaliekwright@yahoo.com>
2020-01-24 21:37:10 -0500

    custom middleware

--- NodeServer/Routes/actionRoutes.js
+++ NodeServer/Routes/actionRoutes.js
@@ -18,4 +18,29 @@ router.use(express.json());
         })
     })
 
+
+//custom middleware
+function validateAction(req,res,next) {
+    const data = req.body;
+    if(!data){
+        res.status(400).json({errorMessage: 'missing action data'})
+    } else if(!data.description || !data.notes) {
+        res.status(400).json({errorMessage:'missing required description and/or notes'})
+    } else {
+        next();
+    }
+}
+
+function validateActionById(req, res, next) {
+    const id = req.params.id;
+    actionDb.get(id)
+    .then(action => {
+        if(!action) {
+            res.status(404).json({error: "The specified ID does not exist."})
+        } else {
+            next();
+        }
+    })
+}
+
     module.exports = router;
